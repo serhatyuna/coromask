b041096f5693ed1571110e15d6cd9e428c86ab0d
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-03-17 16:41:19 +0300

    design  +remove unused images +extension check

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path')
 const multer = require('multer')
 const sharp = require('sharp')
+const fs = require('fs')
 const PORT = process.env.PORT || 5000
 
 require('@tensorflow/tfjs-node');
@@ -42,6 +43,19 @@ express()
         console.log('hata');
       } else {
         console.log(req.file)
+        fs.readdir('./public/images', (err, files) => {
+          for (const file of files) {
+            if (file !== '.keep') {
+              const mtime = fs.statSync(path.join('./public/images', file)).mtime
+              const now = new Date().getTime()
+              if (now - new Date(mtime).getTime() >= 600000) {
+                fs.unlink(path.join('./public/images', file), err => {
+                  if (err) console.log('err')
+                });
+              }
+            }
+          }
+        });
         loadImage(req.file.path).then(async (image) => {
           const detectionWithLandmarks = await faceapi.detectSingleFace(image,new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(useTinyModel)
           if (!detectionWithLandmarks) {
@@ -49,19 +63,22 @@ express()
           }
           let mouth = detectionWithLandmarks.landmarks.getMouth();
           let jawOutline = detectionWithLandmarks.landmarks.getJawOutline();
+          if (!mouth || !jawOutline) {
+            return res.status(400).json({ error: 'Something went wrong! Please try again with another photo.' })
+          }
 
           let minX = Math.min.apply(Math, jawOutline.map(function (o) { return o._x; }));
           let maxX = Math.max.apply(Math, jawOutline.map(function (o) { return o._x; }));
           let minY = Math.min.apply(Math, mouth.map(function (o) { return o._y; }));
           let size = maxX - minX;
           size*=0.9
-          let maskPath = './public/images/mask' + new Date().getTime() + '.png';
+          let maskPath = './public/images/mask-' + new Date().getTime() + '.png';
 
           let height =0;
           sharp('./public/mask.png').resize(Math.floor(size)).toFile(maskPath).then(() => {
             sharp(maskPath).metadata().then((metadata)=>{
               height = metadata.height;
-              let resultPath = './public/images/result' + new Date().getTime() + '.jpg';
+              let resultPath = './public/images/result-' + new Date().getTime() + '.jpg';
             sharp(req.file.path)
             .composite([{
               input: maskPath,
--- public/main.js
+++ public/main.js
@@ -1,13 +1,30 @@
 let form = document.querySelector('.fileForm');
-let imageFile = document.querySelector('.file');
+let imageFile = document.querySelector('.fileInput');
 let image = document.querySelector('.image');
 let errorP = document.querySelector('.error');
+let fileName = document.querySelector('.file-name');
+let loader = document.querySelector('.loader-wrapper');
 let fileList;
 
-form.addEventListener("submit", handleSubmit, false);
+form.addEventListener('submit', handleSubmit, false);
+imageFile.addEventListener('change', handleInputChange);
+
+const exts = ['png', 'jpg', 'jpeg'];
 
 function handleSubmit(e) {
   e.preventDefault();
+
+  errorP.innerHTML = '';
+  image.src = '';
+
+  if (!hasExtension(exts)) {
+    errorP.innerHTML = 'Please upload a valid image file!';
+    fileName.innerHTML = '';
+    form.reset();
+    return false;
+  }
+
+  loader.classList.add('is-active');
 
   let formData = new FormData();
   formData.append("image", imageFile.files[0]);
@@ -21,14 +38,32 @@ function handleSubmit(e) {
     console.log(response.data.b64)
     let path = response.data.b64;
     path = path.substring(9,path.length);
-    errorP.innerHTML = '';
     image.src = path;
   })
   .catch(function (error) {
     errorP.innerHTML = error.response.data.error;
-    image.src = '';
+  })
+  .finally(function () {
+    loader.classList.remove('is-active');
   });
 
   form.reset();
   return false;
+}
+
+function handleInputChange() {
+  let fullPath = imageFile.value;
+  if (fullPath) {
+    let startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+    let filename = fullPath.substring(startIndex);
+    if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+      filename = filename.substring(1);
+    }
+    fileName.innerHTML = filename;
+  }
+}
+
+function hasExtension(exts) {
+  let fileName = imageFile.value;
+  return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(fileName);
 }
