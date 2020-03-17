25b4666176104df392656ab01ddb24cbc651c659
serhatyuna <serhat.yuna@ceng.deu.edu.tr>
2020-03-17 17:12:26 +0300

    formatting

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ express()
   .post("/upload", function (req, res, err) {
     upload(req, res, async (err) => {
       if (err) {
-        console.log('hata');
+        console.log('error');
       } else {
         console.log(req.file)
         fs.readdir('./public/images', (err, files) => {
@@ -56,6 +56,7 @@ express()
             }
           }
         });
+
         loadImage(req.file.path).then(async (image) => {
           const detectionWithLandmarks = await faceapi.detectSingleFace(image,new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks(useTinyModel)
           if (!detectionWithLandmarks) {
@@ -70,33 +71,37 @@ express()
           let minX = Math.min.apply(Math, jawOutline.map(function (o) { return o._x; }));
           let maxX = Math.max.apply(Math, jawOutline.map(function (o) { return o._x; }));
           let minY = Math.min.apply(Math, mouth.map(function (o) { return o._y; }));
-          let size = maxX - minX;
-          size*=0.9
+          let size = (maxX - minX) * 0.9;
           let maskPath = './public/images/mask-' + new Date().getTime() + '.png';
-
           let height =0;
-          sharp('./public/mask.png').resize(Math.floor(size)).toFile(maskPath).then(() => {
-            sharp(maskPath).metadata().then((metadata)=>{
-              height = metadata.height;
-              let resultPath = './public/images/result-' + new Date().getTime() + '.jpg';
-            sharp(req.file.path)
-            .composite([{
-              input: maskPath,
-              top: Math.floor(minY-height/2),
-              left: Math.floor(minX+(size*0.1))
-            }])
-            .jpeg()
-            .toFile(resultPath)
+
+          sharp('./public/mask.png')
+            .resize(Math.floor(size))
+            .toFile(maskPath)
             .then(() => {
-              res.status(200).json({ b64: resultPath })
-            })
-            })
-
+              sharp(maskPath)
+                .metadata()
+                .then((metadata) => {
+                  height = metadata.height;
+                  let resultPath = './public/images/result-' + new Date().getTime() + '.jpg';
+                  sharp(req.file.path)
+                  .composite([{
+                    input: maskPath,
+                    top: Math.floor(minY-height/2),
+                    left: Math.floor(minX+(size*0.1))
+                  }])
+                  .jpeg()
+                  .toFile(resultPath)
+                  .then(() => {
+                    res.status(200).json({ b64: resultPath })
+                  })
+                })
           });
-
         })
-
       }
     })
+  })
+  .use(function (req, res) {
+    res.sendFile(path.join(__dirname, './public/404.html'));
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
